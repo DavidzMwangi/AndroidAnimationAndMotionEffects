54dc5a94472e02c3586ebb617d2822d0690f0d4b
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-10-03 13:05:47 +0300

    fading in

--- app/src/main/java/ke/co/neverest/animations/MainActivity.kt
+++ app/src/main/java/ke/co/neverest/animations/MainActivity.kt
@@ -2,11 +2,55 @@ package ke.co.neverest.animations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),Animation.AnimationListener {
+
+
+    lateinit var txtMessage:TextView
+    lateinit var btn: Button
+    lateinit var animFadein: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        txtMessage=findViewById<TextView>(R.id.text)
+        btn=findViewById(R.id.btn)
+
+
+        //loading animation
+        animFadein=AnimationUtils.loadAnimation(applicationContext,R.anim.fade_in)
+        animFadein.setAnimationListener(this)
+
+        //handling animation on button click
+        btn.setOnClickListener{
+            txtMessage.visibility= View.VISIBLE
+
+            //starting the animation
+            txtMessage.startAnimation(animFadein)
+        }
+
+
     }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
